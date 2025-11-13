fa89c68ce84e6aaf4260ad60be79c22bed36c0f3
Louis McPhee <louismcphee286@gmail.com>
2025-11-13 12:03:45 +0000

    New Button test webpage: JavaScript adventures #1

---
+++ WebDev/Button_Test/script.js
@@ -0,0 +1,10 @@
+const button_1 = document.querySelector("#button_1");
+const heading_1 = document.querySelector("#heading_1");
+
+let count = 1;
+
+button_1.onclick = () =>{
+    button_1.textContent = "Try Again!";
+    heading_1.textContent = "${count} clicks so far";
+    count += 1;
+}
