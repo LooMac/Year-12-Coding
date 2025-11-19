fa2510ae5235288a82a071aaf31855519eecccb3
Louis McPhee <louismcphee286@gmail.com>
2025-11-19 10:54:24 +0000

    Add files via upload

--- WebDev/Button_Test/script.js
+++ WebDev/Button_Test/script.js
@@ -4,7 +4,14 @@ const heading_1 = document.querySelector("#heading_1");
 let count = 1;
 
 button_1.onclick = () =>{
-    button_1.textContent = "Try Again!";
-    heading_1.textContent = `${count} clicks so far`;
-    count += 1;
+    if (count < 10){
+        button_1.textContent = "Try Again!";
+        heading_1.textContent = `${count} clicks so far`;
+        count += 1;
+    }else{
+        button_1.style.backgroundColor = "red";
+        button_1.textContent = "Too Many Clicks";
+        heading_1.textContent = `${count} clicks`;
+    }
+
 };
